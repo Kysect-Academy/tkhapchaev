c64193bddc787e080f19d9e355fca6fed2bed48b
Timur Khapchaev <89847233+tkhapchaev@users.noreply.github.com>
2022-07-10 12:41:31 +0300

    fix: changed methods to non-static

--- Homework 1/Executor.cs
+++ Homework 1/Executor.cs
@@ -1,30 +1,32 @@
-﻿using System;
+using System;
 using System.IO;
 
 namespace Homework1
 {
     class Executor
     {
-        public static string[] GetFileNames()
+        private JsonParser _parser = new JsonParser();
+        
+        public string[] GetFileNames()
         {
-            var directory = JsonParser.GetPath();
+            var directory = _parser.GetPath();
             string[] files = Directory.GetFiles(directory);
             return files;
         }
 
-        public static void Compare()
+        public void Compare()
         {
             try
             {
                 var filesToCheck = GetFileNames();
-                var resultingFile = JsonParser.GetResultingFileName();
+                var resultingFile = _parser.GetResultingFileName();
 
                 for (int i = 0; i < filesToCheck.Length; ++i)
                 {
                     for (int j = i + 1; j < filesToCheck.Length; ++j)
                     {
                         string file1 = filesToCheck[i], file2 = filesToCheck[j];
-                        var similarityPercentage = Comparator.CompareFiles(file1, file2);
+                        var similarityPercentage = new Comparator().CompareFiles(file1, file2);
                         string fileName1 = Path.GetFileName(file1), fileName2 = Path.GetFileName(file2);
                         
                         File.AppendAllText(resultingFile, "Процент схожести файлов ");
