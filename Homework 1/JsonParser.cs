8f644de915f2243a8c8127e1615d0dba9c3c56da
Timur Khapchaev <89847233+tkhapchaev@users.noreply.github.com>
2022-07-11 17:34:24 +0300

    fix: corrected method behavior with null-string

--- Homework 1/JsonParser.cs
+++ Homework 1/JsonParser.cs
@@ -1,24 +1,20 @@
-using System.IO;
 using Microsoft.Extensions.Configuration;
 
-namespace Homework1
+namespace Homework1;
+
+public class JsonParser
 {
-    class JsonParser
+    private IConfigurationRoot _configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json").Build();
+
+    public string GetPath()
     {
-        private IConfigurationRoot _configuration;
+        return _configuration["Working directory:Path"] ?? throw new ArgumentNullException();
+    }
 
-        public string GetPath()
-        {
-            _configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json").Build();
-
-            return _configuration["Working directory:Path"];
-        }
-
-        public string GetResultingFileName()
-        {
-            return _configuration["Resulting file:Name"];
-        }
+    public string GetResultingFileName()
+    {
+        return _configuration["Resulting file:Name"] ?? throw new ArgumentNullException();
     }
 }
