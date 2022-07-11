66b7c5e6930e9b8f616d6fd9f8fd12e5a2854ad5
Timur Khapchaev <89847233+tkhapchaev@users.noreply.github.com>
2022-07-11 17:32:51 +0300

    feat: added return codes

--- Homework 1/Executor.cs
+++ Homework 1/Executor.cs
@@ -1,47 +1,52 @@
-using System;
-using System.IO;
+using System.Security;
 
-namespace Homework1
+namespace Homework1;
+
+public class Executor
 {
-    class Executor
+    private JsonParser _parser = new();
+
+    public string[] GetFileNames()
     {
-        private JsonParser _parser = new JsonParser();
-        
-        public string[] GetFileNames()
+        var directory = _parser.GetPath();
+        string[] files = Directory.GetFiles(directory);
+        return files;
+    }
+
+    public int Compare()
+    {
+        try
         {
-            var directory = _parser.GetPath();
-            string[] files = Directory.GetFiles(directory);
-            return files;
+            var filesToCheck = GetFileNames();
+            var resultingFile = _parser.GetResultingFileName();
+
+            for (int i = 0; i < filesToCheck.Length; ++i)
+            {
+                for (int j = i + 1; j < filesToCheck.Length; ++j)
+                {
+                    string file1 = filesToCheck[i], file2 = filesToCheck[j];
+                    var similarityPercentage = new Comparator().CompareFiles(file1, file2);
+                    string fileName1 = Path.GetFileName(file1), fileName2 = Path.GetFileName(file2);
+
+                    File.AppendAllText(resultingFile, "Процент схожести файлов ");
+                    string result = fileName1 + " и " + fileName2 + ": " + similarityPercentage + "\n";
+                    File.AppendAllText(resultingFile, result);
+                }
+            }
+
+            File.AppendAllText(resultingFile, "--- " + DateTime.Now + " ---" + "\n\n");
+            return 0;
         }
 
-        public void Compare()
+        catch (Exception e)
         {
-            try
+            if (e is UnauthorizedAccessException ||
+                e is SecurityException)
             {
-                var filesToCheck = GetFileNames();
-                var resultingFile = _parser.GetResultingFileName();
-
-                for (int i = 0; i < filesToCheck.Length; ++i)
-                {
-                    for (int j = i + 1; j < filesToCheck.Length; ++j)
-                    {
-                        string file1 = filesToCheck[i], file2 = filesToCheck[j];
-                        var similarityPercentage = new Comparator().CompareFiles(file1, file2);
-                        string fileName1 = Path.GetFileName(file1), fileName2 = Path.GetFileName(file2);
-                        
-                        File.AppendAllText(resultingFile, "Процент схожести файлов ");
-                        string result = fileName1 + " и " + fileName2 + " :" + similarityPercentage + "\n";
-                        File.AppendAllText(resultingFile, result);
-                    }
-                }
-
-                File.AppendAllText(resultingFile, "--- " + DateTime.Now + " ---" + "\n");
+                return -2;
             }
 
-            catch (Exception failure)
-            {
-                Console.WriteLine("Кажется, что-то пошло не так...\n" + failure.Message);
-            }
+            return -1;
         }
     }
 }
