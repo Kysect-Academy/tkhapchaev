a858266405f04c5543b6c879af4b70cb77b6bad9
Timur Khapchaev <338980@niuitmo.ru>
2022-07-17 21:40:02 +0300

    feat: added support for the new appsettings.json structure

--- KysectAcademyTask/JsonParser.cs
+++ KysectAcademyTask/JsonParser.cs
@@ -4,17 +4,57 @@ namespace KysectAcademyTask;
 
 public class JsonParser
 {
-    private IConfigurationRoot _configuration = new ConfigurationBuilder()
+    private readonly IConfigurationRoot _configuration = new ConfigurationBuilder()
         .SetBasePath(Directory.GetCurrentDirectory())
         .AddJsonFile("appsettings.json").Build();
 
-    public string GetPath()
+    public string GetInputDirectoryPath()
     {
-        return _configuration["Working directory:Path"] ?? throw new ArgumentNullException();
+        return _configuration["Input directory path"] ?? throw new ArgumentNullException();
     }
 
-    public string GetResultingFileName()
+    public string GetConsoleOutputStatus()
     {
-        return _configuration["Resulting file:Name"] ?? throw new ArgumentNullException();
+        return _configuration["Report:Console output:Status"] ?? throw new ArgumentNullException();
+    }
+
+    public string GetFileOutputStatus()
+    {
+        return _configuration["Report:File output:Status"] ?? throw new ArgumentNullException();
+    }
+
+    public string GetReportFilePath()
+    {
+        return _configuration["Report:File output:Path"] ?? throw new ArgumentNullException();
+    }
+
+    public string GetReportFileName()
+    {
+        return _configuration["Report:File output:Name"] ?? throw new ArgumentNullException();
+    }
+
+    public string GetReportFileType()
+    {
+        return _configuration["Report:File output:Type"] ?? throw new ArgumentNullException();
+    }
+
+    public string GetExtensionWhitelist()
+    {
+        return _configuration["File filters:Extension whitelist"] ?? throw new ArgumentNullException();
+    }
+
+    public string GetDirectoryBlacklist()
+    {
+        return _configuration["File filters:Directory blacklist"] ?? throw new ArgumentNullException();
+    }
+
+    public string GetAuthorWhitelist()
+    {
+        return _configuration["Author filters:Whitelist"] ?? throw new ArgumentNullException();
+    }
+
+    public string GetAuthorBlacklist()
+    {
+        return _configuration["Author filters:Blacklist"] ?? throw new ArgumentNullException();
     }
 }
