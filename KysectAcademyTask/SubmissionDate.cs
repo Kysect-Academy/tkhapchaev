7b4655e9058c4b90200136edd7af26de48b66d2f
Timur Khapchaev <338980@niuitmo.ru>
2022-07-23 21:27:48 +0300

    fix: changed date convertion

--- KysectAcademyTask/SubmissionDate.cs
+++ KysectAcademyTask/SubmissionDate.cs
@@ -4,19 +4,11 @@ public class SubmissionDate
 {
     public DateTime? ConvertedDate;
 
-    public string? RawDate;
+    public readonly string RawDate;
 
-    public void Parse(string date)
+    public SubmissionDate(string date)
     {
         RawDate = date;
-
-        int year = Convert.ToInt32(date.Substring(0, 4)),
-            month = Convert.ToInt32(date.Substring(4, 2)),
-            day = Convert.ToInt32(date.Substring(6, 2)),
-            hours = Convert.ToInt32(date.Substring(8, 2)),
-            minutes = Convert.ToInt32(date.Substring(10, 2)),
-            seconds = Convert.ToInt32(date.Substring(12, 2));
-
-        ConvertedDate = new DateTime(year, month, day, hours, minutes, seconds);
+        ConvertedDate = DateTime.ParseExact(date, "yyyyMMddHHmmss", System.Globalization.CultureInfo.InvariantCulture);
     }
 }
